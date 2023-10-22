e004305bf7fc790040deeae3dd837eb5deb42289
Vidit <viditk0812@gmail.com>
2023-10-22 16:37:33 +0530

    Improved Mobile View

--- src/Home.js
+++ src/Home.js
@@ -4,10 +4,12 @@ import "./Home.css";
 import vector from "./Homepage_vector.png";
 import cube from "./Black cube 2-02 1.png";
 import { Link } from "react-scroll";
+// import DevComLogo from "./DC 3.svg";
 
 function Home() {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const [blurAmount, setBlurAmount] = useState(0);
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   useEffect(() => {
     const handleResize = () => {
@@ -50,61 +52,113 @@ function Home() {
               aria-controls="navbarNavAltMarkup"
               aria-expanded="false"
               aria-label="Toggle navigation"
+              onClick={() => setIsNavOpen(!isNavOpen)}
             >
-              <span class="navbar-toggler-icon"></span>
+              {isNavOpen ? (
+                <svg
+                  width="28"
+                  height="28"
+                  viewBox="0 0 28 28"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.79997 28.0001L-3.05176e-05 25.2001L11.2 14.0001L-3.05176e-05 2.80006L2.79997 6.10352e-05L14 11.2001L25.2 6.10352e-05L28 2.80006L16.8 14.0001L28 25.2001L25.2 28.0001L14 16.8001L2.79997 28.0001Z"
+                    fill="white"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  width="48"
+                  height="48"
+                  viewBox="0 0 48 48"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_825_211)">
+                    <path
+                      d="M6 9.97949H42V13.9712H6V9.97949ZM6 21.9548H42V25.9465H6V21.9548ZM6 33.93H42V37.9218H6V33.93Z"
+                      fill="#F2F2F2"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_825_211">
+                      <rect width="48" height="48" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              )}
             </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div class="navbar-nav">
-                <Link
-                  className="nav-link"
-                  activeClass="active-link"
-                  to="home"
-                  spy={true}
-                  smooth={true}
-                  duration={500}
-                >
-                  Home
-                </Link>
-                <Link
-                  className="nav-link"
-                  activeClass="active-link"
-                  to="about"
-                  spy={true}
-                  smooth={true}
-                  duration={500}
-                >
-                  About
-                </Link>
-                <Link
-                  className="nav-link"
-                  activeClass="active-link"
-                  to="projects"
-                  spy={true}
-                  smooth={true}
-                  duration={500}
-                >
-                  Projects
-                </Link>
-                <Link
-                  className="nav-link"
-                  activeClass="active-link"
-                  to="team"
-                  spy={true}
-                  smooth={true}
-                  duration={500}
-                >
-                  Team
-                </Link>
-                <Link
-                  className="nav-link"
-                  activeClass="active-link"
-                  to="connect"
-                  spy={true}
-                  smooth={true}
-                  duration={500}
-                >
-                  Connect
-                </Link>
+            <div
+              class="collapse navbar-collapse"
+              id="navbarNavAltMarkup"
+              style={{
+                height: screenWidth < 768 && isNavOpen ? "100vh" : "auto",
+              }}
+            >
+              <div
+                style={{
+                  backgroundColor:
+                    screenWidth < 768 && isNavOpen ? "black" : "transparent",
+                  height: screenWidth < 768 ? "100vh" : "",
+                  width: "100%",
+                }}
+              >
+                <div class="navbar-nav">
+                  <Link
+                    className="nav-link"
+                    activeClass="active-link"
+                    to="home"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    className="nav-link"
+                    activeClass="active-link"
+                    to="about"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                  >
+                    About
+                  </Link>
+                  <Link
+                    className="nav-link"
+                    activeClass="active-link"
+                    to="projects"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                  >
+                    Projects
+                  </Link>
+                  <Link
+                    className="nav-link"
+                    activeClass="active-link"
+                    to="team"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                  >
+                    Team
+                  </Link>
+                  <Link
+                    className="nav-link"
+                    activeClass="active-link"
+                    to="connect"
+                    spy={true}
+                    smooth={true}
+                    duration={500}
+                  >
+                    Connect
+                  </Link>
+                </div>
+                {/* <div className="devcom-svg-container">
+                  <img src={DevComLogo} alt="Devcom Logo" />
+                </div> */}
               </div>
             </div>
           </div>
--- src/ourTeam.js
+++ src/ourTeam.js
@@ -4,10 +4,14 @@ import { Link } from "react-router-dom";
 import TeamMember from "./Teammember.js";
 import record from "./record.json";
 import plus from "./add.png";
+import close from "./close.png";
 
 function OurTeam() {
   const [screenWidth, setScreenWidth] = useState(window.innerWidth);
   const [expandedSections, setExpandedSections] = useState({});
+  const [blurAmount, setBlurAmount] = useState(0);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
 
   function importAll(r) {
     let images = {};
@@ -26,10 +30,17 @@ function OurTeam() {
       setScreenWidth(window.innerWidth);
     };
 
+    const handleScroll = () => {
+      const newBlur = Math.min(window.scrollY / 100, 4);
+      setBlurAmount(newBlur);
+    };
+
     window.addEventListener("resize", handleResize);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
       window.removeEventListener("resize", handleResize);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -38,45 +49,98 @@ function OurTeam() {
       ...prev,
       [sectionName]: !prev[sectionName],
     }));
+    setIsExpanded((prevState) => !prevState);
   };
 
   return (
     <>
       <div className="top-bar"></div>
       <div className="navbar-fixed">
-        <nav className="navbar navbar-expand-md">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="#">
+        <nav
+          class="navbar navbar-expand-md"
+          style={{ backdropFilter: `blur(${blurAmount}px)` }}
+        >
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
               <span className="title">DevCom</span>
             </a>
             <button
-              className="navbar-toggler"
+              class="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarNavAltMarkup"
               aria-controls="navbarNavAltMarkup"
               aria-expanded="false"
               aria-label="Toggle navigation"
+              onClick={() => setIsNavOpen(!isNavOpen)}
             >
-              <span className="navbar-toggler-icon"></span>
+              {isNavOpen ? (
+                <svg
+                  width="28"
+                  height="28"
+                  viewBox="0 0 28 28"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M2.79997 28.0001L-3.05176e-05 25.2001L11.2 14.0001L-3.05176e-05 2.80006L2.79997 6.10352e-05L14 11.2001L25.2 6.10352e-05L28 2.80006L16.8 14.0001L28 25.2001L25.2 28.0001L14 16.8001L2.79997 28.0001Z"
+                    fill="white"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  width="48"
+                  height="48"
+                  viewBox="0 0 48 48"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_825_211)">
+                    <path
+                      d="M6 9.97949H42V13.9712H6V9.97949ZM6 21.9548H42V25.9465H6V21.9548ZM6 33.93H42V37.9218H6V33.93Z"
+                      fill="#F2F2F2"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_825_211">
+                      <rect width="48" height="48" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+              )}
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-              <div className="navbar-nav">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-                <Link className="nav-link" to="/">
-                  About
-                </Link>
-                <Link className="nav-link" to="/">
-                  Projects
-                </Link>
-                <Link className="nav-link" to="/">
-                  Team
-                </Link>
-                <Link className="nav-link" to="/">
-                  Connect
-                </Link>
+            <div
+              class="collapse navbar-collapse"
+              id="navbarNavAltMarkup"
+              style={{
+                height: screenWidth < 768 && isNavOpen ? "100vh" : "auto",
+              }}
+            >
+              <div
+                style={{
+                  backgroundColor:
+                    screenWidth < 768 && isNavOpen ? "black" : "transparent",
+                  height: screenWidth < 768 ? "100vh" : "",
+                  width: "100%",
+                }}
+              >
+                <div class="navbar-nav">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                  <Link className="nav-link" to="/">
+                    About
+                  </Link>
+                  <Link className="nav-link" to="/">
+                    Projects
+                  </Link>
+                  <Link className="nav-link" to="/">
+                    Team
+                  </Link>
+                  <Link className="nav-link" to="/">
+                    Connect
+                  </Link>
+                </div>
               </div>
             </div>
           </div>
@@ -102,8 +166,9 @@ function OurTeam() {
                 <p className="post">{sectionName}</p>
                 {screenWidth < 768 && (
                   <img
-                    src={plus}
-                    alt="Expand"
+                    className="plus_icon"
+                    src={isExpanded ? close : plus}
+                    alt={isExpanded ? "Collapse" : "Expand"}
                     onClick={() => toggleSection(sectionName)}
                   />
                 )}
